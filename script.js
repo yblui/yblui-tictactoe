80bd734f3663ca7f2545f4e9df1367fc6afddbcb
y.b.l** <69856522+yblui@users.noreply.github.com>
2022-08-11 21:37:06 +0800

    3m

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function go(g) {
     }
     document.getElementById(g).classList.add("show");
 }
+animation();
+setInterval(() => {
+    animation();
+}, 18000);
+
+function animation() {
+    for (let i of document.querySelectorAll("#menu use")) {
+        i.setAttribute("y", -150 - Math.random() * 1000);
+    }
+    setTimeout(function () {
+        for (let i of document.querySelectorAll("#menu use")) {
+            i.setAttribute("y", 1000 + Math.random() * 1000);
+        }
+    }, 9000)
+}
