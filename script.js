bb6a1f13f4e086719b9032bff1dccbf7a6500a37
y.b.l** <69856522+yblui@users.noreply.github.com>
2022-08-21 23:00:40 +0800

    2m

--- script.js
+++ script.js
@@ -35,30 +35,40 @@ class Game {
         }
     }
     setBg(scene, fill) {
+        if (!document.getElementById(scene).getElementsByClassName("bg").length) {
+            let bgRect = this.createRect(0, 0, this.width, this.height, fill);
+            bgRect.classList.add("bg");
+            document.getElementById(scene).insertBefore(bgRect, document.getElementById(scene).childNodes[0]);
+        } else {
+            document.getElementById(scene).getElementsByClassName("bg")[0].setAttribute("fill", fill);
+        }
     }
     addSprite(id) {
         let symbolNode = document.createElementNS(SVG_NS, "symbol");
-        let arr = Array.prototype.slice.call(arguments);
-        arr.shift();
-        arr.forEach(function (arg) {
+        let arrArg = Array.prototype.slice.call(arguments);
+        arrArg.shift();
+        arrArg.forEach(function (arg) {
             symbolNode.appendChild(arg);
         });
         symbolNode.setAttribute("id", id);
         document.getElementById(this.id).appendChild(symbolNode);
     }
-    createText(text, x, y, size, fill) {
+    createText(text, x, y, style) {
         let textElement = document.createElementNS(SVG_NS, "text");
         let textNode = document.createTextNode(text);
         textElement.appendChild(textNode);
         textElement.setAttribute("x", x);
         textElement.setAttribute("y", y);
-        textElement.setAttribute("font-size", size);
-        textElement.setAttribute("fill", fill);
         textElement.setAttribute("text-anchor", "middle");
+        if (style) {
+            for (let q of Object.getOwnPropertyNames(style)) {
+                textElement.setAttribute(q, style[q]);
+            }
+        }
         return textElement;
     }
-    addText(id, scene, text, x, y, size, fill) {
-        let textElement = game.createText(text, x, y, size, fill);
+    addText(id, scene, text, x, y, style) {
+        let textElement = this.createText(text, x, y, style);
         textElement.setAttribute("id", id);
         document.getElementById(scene).appendChild(textElement);
     }
@@ -67,21 +77,31 @@ class Game {
         document.getElementById(id).removeChild(document.getElementById(id).childNodes[0]);
         document.getElementById(id).appendChild(textNode);
     }
-    add(id, sprite, scene, x, y) {
+    add(id, sprite, scene, x, y, style) {
         let useNode = document.createElementNS(SVG_NS, "use");
         useNode.setAttributeNS(XLINK_NS, "xlink:href", "#" + sprite);
         useNode.setAttribute("x", x);
         useNode.setAttribute("y", y);
         useNode.setAttribute("id", id);
+        if (style) {
+            for (let q of Object.getOwnPropertyNames(style)) {
+                useNode.setAttribute(q, style[q]);
+            }
+        }
         document.getElementById(scene).appendChild(useNode);
     }
-    createRect(x, y, width, height, fill) {
+    createRect(x, y, width, height, fill, style) {
         let rect = document.createElementNS(SVG_NS, "rect");
         rect.setAttribute("x", x);
         rect.setAttribute("y", y);
         rect.setAttribute("width", width);
         rect.setAttribute("height", height);
         rect.setAttribute("fill", fill);
+        if (style) {
+            for (let q of Object.getOwnPropertyNames(style)) {
+                rect.setAttribute(q, style[q]);
+            }
+        }
         return rect;
     }
     clickEvent(element, func) {
@@ -90,7 +110,25 @@ class Game {
     changeSprite(id, sprite) {
         document.getElementById(id).setAttributeNS(XLINK_NS, "xlink:href", "#" + sprite);
     }
+    transform(id, style) {
+        if (style) {
+            for (let q of Object.getOwnPropertyNames(style)) {
+                document.getElementById(id).setAttribute(q, style[q]);
+            }
+        }
+    }
 }
+Game.prototype.crossTurn = true;
+Game.prototype.crossScore = 0;
+Game.prototype.circleScore = 0;
+Game.prototype.crossWinStreak = 0;
+Game.prototype.circleWinStreak = 0;
+Game.prototype.board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
+Game.prototype.setCount = 1;
 let game = new Game({
     "id": "game",
     "width": 1000,
@@ -98,17 +136,6 @@ let game = new Game({
     "scene": "menu",
     "bg": "white"
 });
-game.crossTurn = true;
-game.crossScore = 0;
-game.circleScore = 0;
-game.crossWinStreak = 0;
-game.circleWinStreak = 0;
-game.board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-];
-game.setCount = 1;
 
 //scenes
 game.addScene("original");
@@ -122,20 +149,58 @@ let rect2 = game.createRect(0, 397, 600, 6, "black");
 let rect3 = game.createRect(197, 0, 6, 600, "black");
 let rect4 = game.createRect(397, 0, 6, 600, "black");
 game.addSprite("board", rect1, rect2, rect3, rect4);
+let rect5 = game.createRect(0, 194 / 3 - 3, 200, 6, "gray");
+let rect6 = game.createRect(0, 194 / 3 * 2 - 3, 200, 6, "gray");
+let rect7 = game.createRect(194 / 3 - 3, 0, 6, 200, "gray");
+let rect8 = game.createRect(194 / 3 * 2 - 3, 0, 6, 200, "gray");
+game.addSprite("boardSm", rect5, rect6, rect7, rect8);
 let blankRect = game.createRect(0, 0, 200, 200, "white");
 game.addSprite("blank", blankRect);
-let oText = game.createText("O", 100, 150, "150px", "black");
+let oText = game.createText("O", 100, 150, {
+    "font-size": "150px",
+    "fill": "black"
+});
 game.addSprite("circle", oText);
-let xText = game.createText("X", 100, 150, "150px", "black");
+let xText = game.createText("X", 100, 150, {
+    "font-size": "150px",
+    "fill": "black"
+});
 game.addSprite("cross", xText);
+let num1 = game.createText("1", 200 / 6, 125, {
+    "font-size": "50px",
+    "fill": "gray"
+})
+game.addSprite("num1", num1);
+let num2 = game.createText("2", 200 / 6, 125, {
+    "font-size": "50px",
+    "fill": "gray"
+})
+game.addSprite("num2", num2);
+let num3 = game.createText("3", 200 / 6, 125, {
+    "font-size": "50px",
+    "fill": "gray"
+})
+game.addSprite("num3", num3);
 
 //menu
 game.p1 = prompt("Player 1:", "Unnamed");
 game.p2 = prompt("Player 2:", "Unnamed");
-game.addText("p1text", "menu", game.p1, 300, 300, "30px", "black");
-game.addText("vs", "menu", "vs.", 500, 300, "30px", "gray");
-game.addText("p2text", "menu", game.p2, 700, 300, "30px", "black");
-game.addText("play", "menu", "Ready >", 500, 500, "30px", "#FF5757");
+game.addText("p1text", "menu", game.p1, 300, 300, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("vs", "menu", "vs.", 500, 300, {
+    "font-size": "30px",
+    "fill": "gray"
+});
+game.addText("p2text", "menu", game.p2, 700, 300, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("play", "menu", "Ready >", 500, 500, {
+    "font-size": "30px",
+    "fill": "#ff5757"
+});
 game.clickEvent("play", function () {
     game.switchScene("original");
 });
@@ -174,8 +239,7 @@ for (let m = 1; m <= 9; m++) {
                     game.circleWinStreak = 0;
                     game.setText("red1", RANDOM_DRAW[Math.floor(RANDOM_DRAW.length * Math.random())]);
                 }
-                game.setText("xscore1", game.crossScore);
-                game.setText("oscore1", game.circleScore);
+                updateScore();
                 setTimeout(function () {
                     game.gameOver = true;
                 }, 0);
@@ -184,12 +248,30 @@ for (let m = 1; m <= 9; m++) {
     });
 }
 game.add("boardUse", "board", "original", 200, 100);
-game.addText("round1", "original", "Round 1", 500, 40, "30px", "black");
-game.addText("red1", "original", "", 500, 70, "20px", "#FF5757");
-game.addText("x1", "original", game.p1, 300, 40, "30px", "black");
-game.addText("o1", "original", game.p2, 700, 40, "30px", "black");
-game.addText("xscore1", "original", game.crossScore, 100, 40, "30px", "#ff5757");
-game.addText("oscore1", "original", game.circleScore, 900, 40, "30px", "#5A9FDB");
+game.addText("round1", "original", "Round 1", 500, 40, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("red1", "original", "", 500, 70, {
+    "font-size": "20px",
+    "fill": "#ff5757"
+});
+game.addText("x1", "original", game.p1, 300, 40, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("o1", "original", game.p2, 700, 40, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("xscore1", "original", game.crossScore, 100, 40, {
+    "font-size": "30px",
+    "fill": "#ff5757"
+});
+game.addText("oscore1", "original", game.circleScore, 900, 40, {
+    "font-size": "30px",
+    "fill": "#5a9fdb"
+});
 game.clickEvent("game", function () {
     if (game.gameOver) {
         game.gameOver = false;
@@ -205,7 +287,7 @@ game.clickEvent("game", function () {
         game.setCount++;
         if (game.setCount == 10) {
             game.switchScene("numbers");
-            game.setCount=1;
+            game.setCount = 1;
         }
     }
 });
@@ -231,10 +313,86 @@ function win() {
         return null;
     }
 }
+function updateScore() {
+    for (let i = 1; i <= 3; i++) {
+        game.setText("xscore" + i, game.crossScore);
+        game.setText("oscore" + i, game.circleScore);
+    }
+}
 
 //numbers
+game.addText("round2", "numbers", "Round 2", 500, 40, {
+    "font-size": "30px",
+    "fill": "black"
+});
+arr.forEach(function (a) {
+    arr.forEach(function (b) {
+        game.add("rectn" + (a / 200 * 3 + b / 200 + 1), "blank", "numbers", b + 200, a + 100);
+    });
+});
+for (let m = 1; m <= 9; m++) {
+    game.clickEvent("rectn" + m, function () {
+        if (!game.board[(m - 1 - (m - 1) % 3) / 3][(m - 1) % 3]) {
+            game.add("num1" + m, "num1", "numbers", (m - 1) % 3 * 200 + 200, (m - 1 - (m - 1) % 3) / 3 * 200 + 100);
+            game.add("num2" + m, "num2", "numbers", (m - 1) % 3 * 200 + 200 + 200 / 3, (m - 1 - (m - 1) % 3) / 3 * 200 + 100);
+            game.add("num3" + m, "num3", "numbers", (m - 1) % 3 * 200 + 200 + 200 / 3 * 2, (m - 1 - (m - 1) % 3) / 3 * 200 + 100);
+            game.board[(m - 1 - (m - 1) % 3) / 3][(m - 1) % 3] = "?";
+            game.clickEvent("num1" + m, function () {
+                game.board[(m - 1 - (m - 1) % 3) / 3][(m - 1) % 3] = ((game.crossTurn) ? "x" : "o") + "1";
+                game.crossTurn = !game.crossTurn;
+            })
+            game.clickEvent("num2" + m, function () {
+                game.board[(m - 1 - (m - 1) % 3) / 3][(m - 1) % 3] = ((game.crossTurn) ? "x" : "o") + "2";
+                game.crossTurn = !game.crossTurn;
+            })
+            game.clickEvent("num3" + m, function () {
+                game.board[(m - 1 - (m - 1) % 3) / 3][(m - 1) % 3] = ((game.crossTurn) ? "x" : "o") + "3";
+                game.crossTurn = !game.crossTurn;
+            })
+        }
+    })
+}
 game.add("boardUsen", "board", "numbers", 200, 100);
+game.addText("red2", "numbers", "", 500, 70, {
+    "font-size": "20px",
+    "fill": "#ff5757"
+});
+game.addText("x2", "numbers", game.p1, 300, 40, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("o2", "numbers", game.p2, 700, 40, {
+    "font-size": "30px",
+    "fill": "black"
+});
+game.addText("xscore2", "numbers", 0, 100, 40, {
+    "font-size": "30px",
+    "fill": "#ff5757"
+});
+game.addText("oscore2", "numbers", 0, 900, 40, {
+    "font-size": "30px",
+    "fill": "#5a9fdb"
+});
 
 //killer
+game.addText("round3", "killer", "Round 3", 500, 40, {
+    "font-size": "30px",
+    "fill": "white"
+});
+game.add("boardUsen", "board", "killer", 200, 100);
+arr.forEach(function (a) {
+    arr.forEach(function (b) {
+        game.add("rect" + (a / 200 * 3 + b / 200 + 1), "boardSm", "killer", b + 200, a + 100);
+    });
+});
+game.setBg("killer", "#111")
+game.addText("xscore3", "killer", 0, 100, 40, {
+    "font-size": "30px",
+    "fill": "#ff5757"
+});
+game.addText("oscore3", "killer", 0, 900, 40, {
+    "font-size": "30px",
+    "fill": "#5a9fdb"
+});
 
 //result
